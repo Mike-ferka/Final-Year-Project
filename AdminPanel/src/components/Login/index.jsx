162c140258dea7041bcc5c19d39f741a307fdb70
Mike-ferka <93842451+Mike-ferka@users.noreply.github.com>
2024-07-08 18:10:35 +0000

    Login updated

--- AdminPanel/src/components/Login/index.jsx
+++ AdminPanel/src/components/Login/index.jsx
@@ -17,7 +17,7 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8080/api/auth";
+			const url = "https://final-year-projet-backend.onrender.com/api/auth";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
 			localStorage.setItem("deviceId", data.deviceId); // Store deviceId
